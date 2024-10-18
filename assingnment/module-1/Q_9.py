c5b78a12b7a66b048d1c15fbe7a1dd9f4fca6125
priyanshigokani <gokanipriyanshi@gmail.com>
2024-10-18 17:30:26 +0530

    assignment 1

--- assingnment/module-1/Q_9.py
+++ assingnment/module-1/Q_9.py
@@ -1,8 +1,10 @@
 #Write a python program to sum of the first n positive integers.
 
 n = int(input("Enter your number: "))
+sum=0
 
-sum=(n *(n+1))/2
+for i in range(n+1):
+    sum+=i
 
 print("Sum: " , sum)
 
